2843f847e59ab98e24af5e0096e72881a5a2f9e7
DanielLai20 <daniel.lai9798@gmail.com>
2023-12-12 14:33:49 -0500

    added navbar and background gradient

---
+++ components/nav.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import Logo from '../public/logos/trackerjet-logo.png';
+
+const Nav = () => {
+    return (
+        <nav className="fixed top-0 left-0 w-full z-10 bg-transparent">
+            <div className="flex justify-start items-center py-4 px-4">
+                <Image
+                    src={Logo}
+                    alt=""
+                    height={75}
+                    width={75}
+                    style={{ margin: "-20px"}}
+                ></Image>
+                <h1 className="text-2xl font-bold text-slate-200">
+                    Trackerjet
+                </h1>
+            </div>
+        </nav>
+    );
+
+};
+
+export default Nav;
